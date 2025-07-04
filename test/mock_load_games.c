fdb7915533d500448d6b4ea972b4af87baa177ba
David <89667110+Tropical7@users.noreply.github.com>
2025-07-04 21:39:37 +0200

    Added mock_load_games for future testing

--- test/mock_load_games.c
+++ test/mock_load_games.c
@@ -1,21 +1,5 @@
 #include "load_games.h"
-#include <stdlib.h>
-#include <string.h>
-
-Game dummy_game_list[1];
 
 Game* load_games(const char *filename, int *count_out) {
-    (void)filename;
-
-    *count_out = 1;
-
-    // Dummy-Spiel setzen
-    strncpy(dummy_game_list[0].title, "Mock Game", sizeof(dummy_game_list[0].title) - 1);
-    strncpy(dummy_game_list[0].description, "Test description", sizeof(dummy_game_list[0].description) - 1);
-    strncpy(dummy_game_list[0].version, "1.0", sizeof(dummy_game_list[0].version) - 1);
-    strncpy(dummy_game_list[0].mode, "Singleplayer", sizeof(dummy_game_list[0].mode) - 1);
-    dummy_game_list[0].id = 1;
-    dummy_game_list[0].current_streams = 0;
-
-    return dummy_game_list;
+    // Für zukünftige Tests implementieren
 }
